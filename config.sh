c76102cbdcbaafc163c8ee0e445be3f85c6e250d
RecoveryBuilder <ci.twrp@gmail.com>
2023-06-01 03:50:31 +0000

    RecoveryBuilder: Trigger Build

--- config.sh
+++ config.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-export TWRP_MANIFEST=https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp
+export TWRP_MANIFEST=https://github.com/ShazuxD/platform_manifest_twrp_aosp
 export TWRP_BRANCH=twrp-12.1
-export DT_LINK=https://github.com/ShazuxD/android_device_xiaomi_tulip
-export DT_BRANCH=twrp-12.1
-export DEVICE_PATH=device/xiaomi/tulip
-export DEVICE=tulip
-export MAKEFILE=twrp_tulip
-export TARGET=recoveryimage
-export OUTPUT=recovery.img
+export DT_LINK=https://github.com/ShazuxD/android_device_xiaomi_fleur-twrp
+export DT_BRANCH=android-12.1
+export DEVICE_PATH=device/xiaomi/fleur
+export DEVICE=fleur
+export MAKEFILE=twrp_fleur
+export TARGET=bootimage
+export OUTPUT=boot.img
 export ALT_FILE=empty
-export RANDOM=16650
+export RANDOM=21180
