8c844e99ffbd30170f14ef75852ae7740b5f315b
RecoveryBuilder <ci.twrp@gmail.com>
2023-06-16 14:48:20 +0000

    RecoveryBuilder: Trigger Build

--- config.sh
+++ config.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-export TWRP_MANIFEST=https://github.com/ShazuxD/platform_manifest_twrp_aosp
+export TWRP_MANIFEST=https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git
 export TWRP_BRANCH=twrp-12.1
-export DT_LINK=https://github.com/ShazuxD/android_device_xiaomi_fleur-twrp
-export DT_BRANCH=android-12.1
-export DEVICE_PATH=device/xiaomi/fleur
-export DEVICE=fleur
-export MAKEFILE=twrp_fleur
-export TARGET=bootimage
-export OUTPUT=boot.img
+export DT_LINK=https://github.com/perilouspike/android_device_rockchip_rk3328_box.git
+export DT_BRANCH=A12
+export DEVICE_PATH=device/rockchip/rk3328_box
+export DEVICE=rk3328_box
+export MAKEFILE=twrp_rk3328_box
+export TARGET=recoveryimage
+export OUTPUT=recovery.img
 export ALT_FILE=empty
-export RANDOM=21180
+export RANDOM=26037
