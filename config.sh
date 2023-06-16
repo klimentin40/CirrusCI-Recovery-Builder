f2e18912c52844e123812e80265d3088bc9afc43
RecoveryBuilder <ci.twrp@gmail.com>
2023-06-16 20:19:30 +0000

    RecoveryBuilder: Trigger Build

--- config.sh
+++ config.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 export TWRP_MANIFEST=https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp
-export TWRP_BRANCH=twrp-12.1
-export DT_LINK=https://github.com/perilouspike/android_device_rockchip_rk3328_box.git
-export DT_BRANCH=A12
-export DEVICE_PATH=device/rockchip/rk3328_box
-export DEVICE=rk3328_box
-export MAKEFILE=twrp_rk3328_box
-export TARGET=recoveryimage
-export OUTPUT=recovery.img
+export TWRP_BRANCH=twrp-11
+export DT_LINK=https://github.com/perilouspike/recovery_device_xiaomi_selene.git
+export DT_BRANCH=a11-r
+export DEVICE_PATH=device/xiaomi/selene
+export DEVICE=selene
+export MAKEFILE=twrp_selene
+export TARGET=bootimage
+export OUTPUT=boot.img
 export ALT_FILE=empty
-export RANDOM=7857
+export RANDOM=14164
