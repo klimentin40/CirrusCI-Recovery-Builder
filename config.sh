85e16468192cc48de2b503bdb4d69aac6e503271
RecoveryBuilder <ci.twrp@gmail.com>
2023-06-18 07:01:53 +0000

    RecoveryBuilder: Trigger Build

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export TWRP_MANIFEST=https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp
-export TWRP_BRANCH=twrp-12.1
+export TWRP_BRANCH=twrp-11
 export DT_LINK=https://github.com/perilouspike/recovery_device_xiaomi_selene.git
 export DT_BRANCH=a11-r
 export DEVICE_PATH=device/xiaomi/selene
@@ -9,4 +9,4 @@ export MAKEFILE=twrp_selene
 export TARGET=bootimage
 export OUTPUT=boot.img
 export ALT_FILE=empty
-export RANDOM=25662
+export RANDOM=14279
