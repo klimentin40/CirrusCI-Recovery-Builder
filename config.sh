1023a2f6b91fb3186a12f3093358918eed214b36
RecoveryBuilder <ci.twrp@gmail.com>
2023-06-25 08:12:37 +0000

    RecoveryBuilder: Trigger Build

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-export TWRP_MANIFEST=https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git
+export TWRP_MANIFEST=https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp
 export TWRP_BRANCH=twrp-12.1
-export DT_LINK=https://github.com/klimentin40/android_device_rockchip_rk3328_box.git
+export DT_LINK=https://github.com/klimentin40/android_device_rockchip_rk3328_box
 export DT_BRANCH=A12
 export DEVICE_PATH=device/rockchip/rk3328_box
 export DEVICE=rk3328_box
@@ -9,4 +9,4 @@ export MAKEFILE=twrp_rk3328_box
 export TARGET=recoveryimage
 export OUTPUT=recovery.img
 export ALT_FILE=empty
-export RANDOM=12337
+export RANDOM=11152
