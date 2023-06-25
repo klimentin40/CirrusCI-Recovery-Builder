28065149ed8bb8411509fb1ae48b4d0d4a39d849
klimentin40 <38346731+klimentin40@users.noreply.github.com>
2023-06-25 09:45:26 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export TWRP_MANIFEST=https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp
 export TWRP_BRANCH=twrp-12.1
-export DT_LINK=https://github.com/klimentin40/android_device_rockchip_rk3328_box
+export DT_LINK=https://github.com/klimentin40/android_device_rockchip_rk3328_box.git
 export DT_BRANCH=A12
 export DEVICE_PATH=device/rockchip/rk3328_box
 export DEVICE=rk3328_box
